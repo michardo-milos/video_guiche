c5f7f44caa3e534f61e67b77ef862ed19085248e
miguelferreira4747 <Hqghryb123>
2021-01-04 19:37:08 +0000

    First Commit

---
+++ app/src/main/java/com/example/videoapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.videoapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import org.jitsi.meet.sdk.JitsiMeet
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import java.net.MalformedURLException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize default options for Jitsi Meet conferences.
+        val serverURL: URL
+        serverURL = try {
+            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
+            URL("https://188.166.13.78")
+            // URL("https://meet.jit.si")
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            throw RuntimeException("Invalid server URL!")
+        }
+        val defaultOptions = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(serverURL)
+            // When using JaaS, set the obtained JWT here
+            //.setToken("MyJWT")
+            .setWelcomePageEnabled(true)
+            .build()
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions)
+    }
+
+    fun onButtonClick(v: View?) {
+        val editText = findViewById<EditText>(R.id.conferenceName)
+        val text = editText.text.toString()
+        if (text.isNotEmpty()) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            val options = JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build()
+            Log.d("JITSI_LOG", text)
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            // JitsiMeetActivity.launch(this, options)
+            JitsiMeetActivity.launch(this, options)
+
+        }
+    }
+}
